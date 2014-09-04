bd96125ce7cf9d04fd996dcf56ece4104d5af901
Thinking Song <123wuming_123@163.com>
2014-09-04 21:37:29 +0800

    修改网站首页

---
+++ protected/views/site/index.php
@@ -0,0 +1,10 @@
+ <div style="margin-bottom:10px;" id="ordct606">
+    <div style="padding-bottom:10px;border:solid 1px #D9E0E6;">
+       <div class="b606">学校简介</div>
+       <div style="padding-right:4px;padding-top:7px;padding-left:5px;">
+            <div style="padding:10px;line-height:180%">
+            </div>
+        </div>   
+    </div>
+</div>
+   

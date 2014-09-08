53e382383739b43ea705f23eece785deb16f46eb
Thinking Song <123wuming_123@163.com>
2014-09-08 10:05:31 +0800

    解决前台不需要加载bootstrap的问题

--- protected/modules/admin/AdminModule.php
+++ protected/modules/admin/AdminModule.php
@@ -16,6 +16,8 @@ class AdminModule extends CWebModule
 
 	public function beforeControllerAction($controller, $action)
 	{
+		Yii::app()->booster;
+
 		if(parent::beforeControllerAction($controller, $action))
 		{
 			// this method is called before any module controller action is performed
